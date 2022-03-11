5cd2caea2e79f931aab6ebd1f1e8d48d8a4d9687
Sojung Yoon <han3338@hansung.ac.kr>
2022-03-12 02:23:16 +0900

    ysj

--- board-front/board-app/src/components/Headers/IndexHeader.js
+++ board-front/board-app/src/components/Headers/IndexHeader.js
@@ -53,7 +53,7 @@ function IndexHeader() {
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
-              Be Vegin jyy gain pjh
+              Be Vegin jyy gain pjh ysj
             </h2>
           </Container>
         </div>
