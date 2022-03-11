21b61261c836f8776e5e0d9bc1a81a11ff375f19
juhye <yoyoung675@gmail.com>
2022-03-12 01:23:31 +0900

    juhye

--- board-front/board-app/src/components/Headers/IndexHeader.js
+++ board-front/board-app/src/components/Headers/IndexHeader.js
@@ -53,7 +53,7 @@ function IndexHeader() {
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
-              Be Vegin jyy gain
+              Be Vegin jyy gain juhye
             </h2>
           </Container>
         </div>
