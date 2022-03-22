02a412741bf2d96ec8b87089f868e684b5a207c1
juhye <yoyoung675@gmail.com>
2022-03-22 12:43:56 +0900

    juhye home magazine design

--- board-front/board-app/src/views/index-sections/Magazine.js
+++ board-front/board-app/src/views/index-sections/Magazine.js
@@ -1,13 +1,92 @@
-import React, { Component } from 'react';
+import React from "react";
+import {
+  StackedCarousel,
+  ResponsiveContainer
+} from "react-stacked-center-carousel";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import "assets/css/Slide.css";
+import { Slide } from "./Slide";
 
-class Magazine extends Component {
-    render() {
-        return (
-            <>
-               <p>매거진 부분</p> 
-            </>
-        );
-    }
-}
+const data = [
+  {
+    image: "https://assets.vogue.com/photos/61bd0b4a1b84e62f58b4d5a1/master/w_1920,c_limit/VO0222_Cover_Logo.jpg",
+    altText: "Somewhere",
+    caption: "Somewhere",
+  },
+  {
+    image: "https://assets.vogue.com/photos/62033851bf3f223aebd2f948/master/w_1600,c_limit/VO0322_SocialCover_logo_R2.jpg",
+    altText: "Somewhere1",
+    caption: "Somewhere1",
+  },
+  {
+    image: "https://assets.vogue.com/photos/61ae16157c13ee66d47c2972/master/w_1920,c_limit/VO0122_SocialCover_logo.jpg",
+    altText: "Somewhere2",
+    caption: "Somewhere2",
+  },
+  {
+    image: require("assets/img/soroush-karimi.jpg"),
+    altText: "Somewhere3",
+    caption: "Somewhere3",
+  },
+  {
+    image: require("assets/img/federico-beccari.jpg"),
+    altText: "Somewhere4",
+    caption: "Somewhere4",
+  },
+  {
+    image: require("assets/img/joshua-stannard.jpg"),
+    altText: "Somewhere5",
+    caption: "Somewhere6",
+  }
+];
+
+const Magazine = () => {
+  const ref = React.useRef(StackedCarousel);
+  return (
+    <>
+    <div className="magazine_title">
+        <h3> VEGIN'S MAGAZINE </h3>
+    </div>
+    <div className="card mx-auto">
+      <div style={{ width: "70%", position: "relative" }}>
+        <ResponsiveContainer
+          carouselRef={ref}
+          render={(width, carouselRef) => {
+            return (
+              <StackedCarousel
+                ref={carouselRef}
+                slideComponent={Slide}
+                slideWidth={300}
+                carouselWidth={width}
+                data={data}
+                maxVisibleSlide={5}
+                disableSwipe
+                customScales={[1, 0.85, 0.7, 0.55]}
+                transitionTime={450}
+              />
+            );
+          }}
+        />
+        <Fab
+          className="card-button left"
+          size="small"
+          onClick={() => ref.current?.goBack()}
+        >
+          <KeyboardArrowLeftIcon style={{ fontSize: 60 }} />
+        </Fab>
+        <Fab
+          className="card-button right"
+          size="small"
+          onClick={() => ref.current?.goNext()}
+        >
+          <KeyboardArrowRightIcon style={{ fontSize: 60 }} />
+        </Fab>
+      </div>
+    </div>
+    </>
+  );
+};
 
 export default Magazine;
---
+++ board-front/board-app/src/views/index-sections/Slide.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "assets/css/Slide.css";
+
+export const Slide = React.memo(function (StackedCarouselSlideProps) {
+  const {
+    data,
+    dataIndex,
+    isCenterSlide,
+    swipeTo,
+    slideIndex
+  } = StackedCarouselSlideProps;
+
+  const coverImage = data[dataIndex].image;
+
+  return (
+    <div className="card-card" draggable={false}>
+      <div className={`cover fill ${isCenterSlide ? "off" : "on"}`}>
+        <div
+          className="card-overlay fill"
+          onClick={() => {
+            if (!isCenterSlide) swipeTo(slideIndex);
+          }}
+        />
+      </div>
+      <div className="detail fill">
+        <div className="discription">
+          <img
+            style={{ width: "100%", height: "100%" }}
+            alt="j"
+            className="cover-image"
+            src={coverImage}
+          />
+        </div>
+      </div>
+    </div>
+  );
+});
