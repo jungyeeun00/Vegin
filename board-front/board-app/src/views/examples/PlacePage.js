a2134588170caa295c5d2e1bb3de778173bf78c6
정예은 <jeong-yeeun@jeong-yeeun-ui-MacBookAir.local>
2022-03-29 01:59:00 +0900

    jyy place insert map

---
+++ board-front/board-app/src/views/examples/PlacePage.js
@@ -0,0 +1,30 @@
+/* global kakao */
+import VeginFooter from 'components/Footers/VeginFooter';
+import IndexNavbar from 'components/Navbars/IndexNavbar';
+import React, { Component, useEffect } from 'react';
+import { Container } from 'reactstrap';
+
+const PlacePage = () => {
+    useEffect(() => {
+        var container = document.getElementById('map');
+        var options = {
+            center: new kakao.maps.LatLng(37.582872968256936, 127.00946831534178),
+            level: 3
+        };
+        var map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <>
+            <IndexNavbar />
+            <Container>
+                <div id="map" style={{width:"100%", height:"800px"}}>
+
+                </div>
+            </Container>
+            <VeginFooter />
+        </>
+    );
+}
+
+export default PlacePage;
